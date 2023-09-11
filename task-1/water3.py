ba2a36d411aae9f1c9032c6607e47dd5b130e447
as32627siit <144674630+as32627siit@users.noreply.github.com>
2023-09-11 22:12:09 +0700

    Update water3.py

--- task-1/water3.py
+++ task-1/water3.py
@@ -1,9 +1,22 @@
 def initial_state():
-    return (0, 0, 0)
+    return (8, 0, 0)
 
 def is_goal(s):
-    return True
+    return s[0,1] == 4
 
 def successors(s):
     x, y, z = s
+    if x>0:
+        yield ((0,y,z), 1)
+    if y>0:
+        yield ((x,0,z), 1)
+    if z>0:
+        yield ((x,y,0),1)
+    if x<8:
+        yield((8,y,z), 8-x)
+    if y<5:
+        yield((x,5,z), 5-y)
+    if z<3:
+        yield((x,y,3), 3-z)
+    
     return []
