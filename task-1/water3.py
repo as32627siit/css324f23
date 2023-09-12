f585be07c19e8d0b8db9dc02756db870654c0176
as57win32 <144674036+as57win32@users.noreply.github.com>
2023-09-12 14:24:02 +0700

    Update water3.py

--- task-1/water3.py
+++ task-1/water3.py
@@ -2,22 +2,40 @@ def initial_state():
     return (8, 0, 0)
 
 def is_goal(s):
-    return s[0,1] == 4
+    return s[0] == 4 and s[1] == 4
 
 def successors(s):
-    x, y, z = s
-    if x>0:
-        yield ((0,y,z), 1)
-    if y>0:
-        yield ((x,0,z), 1)
-    if z>0:
-        yield ((x,y,0),1)
-    if x<8:
-        yield((8,y,z), 8-x)
-    if y<5:
-        yield((x,5,z), 5-y)
-    if z<3:
-        yield((x,y,3), 3-z)
-    
-    return []
-hee tits
+    actions = []
+    a, b, c = s
+
+    # Pour from the 8-liter bottle to the 5-liter bottle
+    if a > 0 and b < 5:
+        amount = min(a, 5 - b)
+        actions.append(((a - amount, b + amount, c), amount))
+
+    # Pour from the 8-liter bottle to the 3-liter bottle
+    if a > 0 and c < 3:
+        amount = min(a, 3 - c)
+        actions.append(((a - amount, b, c + amount), amount))
+
+    # Pour from the 5-liter bottle to the 8-liter bottle
+    if b > 0 and a < 8:
+        amount = min(b, 8 - a)
+        actions.append(((a + amount, b - amount, c), amount))
+
+    # Pour from the 5-liter bottle to the 3-liter bottle
+    if b > 0 and c < 3:
+        amount = min(b, 3 - c)
+        actions.append(((a, b - amount, c + amount), amount))
+
+    # Pour from the 3-liter bottle to the 8-liter bottle
+    if c > 0 and a < 8:
+        amount = min(c, 8 - a)
+        actions.append(((a + amount, b, c - amount), amount))
+
+    # Pour from the 3-liter bottle to the 5-liter bottle
+    if c > 0 and b < 5:
+        amount = min(c, 5 - b)
+        actions.append(((a, b + amount, c - amount), amount))
+
+    return actions
